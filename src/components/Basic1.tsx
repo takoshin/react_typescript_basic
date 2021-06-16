c0a807b17c061c2f87c1ef3334cc05ae22d7e408
Shin Tako <k1x.heart@icloud.com>
2021-06-17 07:19:52 +0900

    fuction component basic

---
+++ src/components/Basic1.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Basic1: () => JSX.Element = () => {
+  return (
+    <>
+      <h1>Hello, React!</h1>
+      <h2>Hello, TypeScript!</h2>
+    </>
+  )
+}
+
+export default Basic1
