9c44cbf02c327ca9d42fef1f5a1364f97e4dc7ab
Shin Tako <k1x.heart@icloud.com>
2021-06-17 07:39:37 +0900

    Props lesson

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Basic1 />
+        <Basic1 name="World" />
       </header>
     </div>
   )
--- src/components/Basic1.tsx
+++ src/components/Basic1.tsx
@@ -1,12 +1,20 @@
 import React from 'react'
 
-const Basic1: () => JSX.Element = () => {
+interface AppName {
+  name?: string
+}
+
+const Basic1: React.FunctionComponent<AppName> = ({ name }) => {
   return (
     <>
-      <h1>Hello, React!</h1>
+      <h1>Hello, {name}!</h1>
       <h2>Hello, TypeScript!</h2>
     </>
   )
+}
+
+Basic1.defaultProps = {
+  name: 'Hello, defaultProps!',
 }
 
 export default Basic1
