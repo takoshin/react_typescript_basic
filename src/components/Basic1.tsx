f9561f1969389167bbf51718bd6eb8bac7a96dda
Shin Tako <k1x.heart@icloud.com>
2021-06-17 20:14:06 +0900

    useState_lesson

--- src/components/Basic1.tsx
+++ src/components/Basic1.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 interface AppName {
   name?: string
@@ -8,10 +8,12 @@ const Basic1: React.FunctionComponent<AppName> = ({ name }) => {
   const clickHandler = (): void => {
     console.log('clicked')
   }
+  const [count, setCount] = useState<number>(0)
 
   return (
     <>
-      <button onClick={clickHandler}>Click</button>
+      <button onClick={() => setCount(count + 1)}>+ {count}</button>
+      <button onClick={() => setCount(count - 1)}>-</button>
       <h1>Hello, {name}!</h1>
       <h2>Hello, TypeScript!</h2>
     </>
